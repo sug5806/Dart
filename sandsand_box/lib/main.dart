476e772fdc82250fcf0669e3fd415e2811505c28
sug5806 <sug5806@gmail.com>
2019-11-12 21:32:13 +0900

    feat: update

--- sandsand_box/lib/main.dart
+++ sandsand_box/lib/main.dart
@@ -1,170 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Layout',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'App Bar Title'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List itemList = [
-    'Card Text 2 Line 1',
-    'Card Text 2 Line 2',
-    'Card Text 2 Line 3',
-    'Card Text 2 Line 3',
-    'Card Text 2 Line 3',
-    'Card Text 2 Line 3',
-    'Card Text 2 Line 3',
-    'Card Text 2 Line 3',
-    'Card Text 2 Line 3',
-
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Sub Title',
-                      style: TextStyle(
-                        fontSize: 25.0,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        side: BorderSide(
-                          width: 3.0,
-                        ),
-                      ),
-                      margin: EdgeInsets.all(15.0),
-                      color: Colors.grey,
-                      elevation: 10.0,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'Card 1 Text',
-                          style: TextStyle(
-                            fontSize: 25.0,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        side: BorderSide(
-                          width: 3.0,
-                        ),
-                      ),
-                      margin: EdgeInsets.all(15.0),
-                      color: Colors.grey,
-                      elevation: 10.0,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'Card 2 Header',
-                                  style: TextStyle(
-                                    fontSize: 25.0,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: List.generate(
-                              itemList.length,
-                                (i) => Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  itemList[i],
-                                  style: TextStyle(
-                                    fontSize: 25.0,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        side: BorderSide(
-                          width: 3.0,
-                        ),
-                      ),
-                      margin: EdgeInsets.all(15.0),
-                      color: Colors.grey,
-                      elevation: 10.0,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'Card 3 Text',
-                          style: TextStyle(
-                            fontSize: 25.0,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ],
+    return new Container(
+      color: Colors.green,
+      child: new Column(
+        children: <Widget>[
+          new RaisedButton(
+            onPressed: () => Navigator.pop(context),
+            child: new Text("back"),
           ),
-        ),
+        ],
       ),
     );
   }
 }
+
+class FirstPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new FirstPageState();
+}
+
+class FirstPageState extends State<FirstPage> {
+
+  Color color = Colors.white;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      color: color,
+      child: new Column(
+        children: <Widget>[
+          new RaisedButton(
+            child: new Text("next"),
+            onPressed: () {
+              Navigator.push(
+                context,
+                new MaterialPageRoute(builder: (context) => new SecondPage()),
+              )
+                .then((value) {
+                setState(() {
+                  color = color == Colors.white ? Colors.grey : Colors.white;
+                });
+              });
+            }),
+        ],
+      ),
+    );
+  }
+}
+
+void main() => runApp(
+  new MaterialApp(
+    builder: (context, child) => new SafeArea(child: child),
+    home: new FirstPage(),
+  ),
+);
